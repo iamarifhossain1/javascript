3115165f3d2364d5adad6f73ddf0d0dbd246845e
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-06-05 02:24:24 +0600

    taxCalculator added

--- hello-js-assignment.js
+++ hello-js-assignment.js
@@ -1,15 +1,15 @@
-function taxCalculator (Harun) {
-   const income = 100000;
-   const expense = 30000;
-   if (expense > income) {
-        return 'Invalid input'
-   }
-   const remainingMoney = income - expense;
-   const taxRate = .20;
-   const totalTax = remainingMoney * taxRate;
-   return totalTax;
-   
-}   
+function taxCalculator (harun) {
+    const income = 100000;
+    const expense = 30000;
+    const remainingMoney = income - expense;
+    if (expense > income) {
+        return 'Invalid input';
+    }
+
+    const taxRate = .20;
+    const totalTax = remainingMoney * taxRate;
+    return totalTax;
+}
 
 const result = taxCalculator ();
-console.log (result);
+console.log ( 'Total tax on remaining money:',result, 'Taka');
